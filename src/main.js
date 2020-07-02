571c398566aa8e68a14f867ce35a8576e9eff156
Martin <2906935779@qq.com>
2020-07-02 09:27:21 +0800

    从vs打开

--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
-import router from './router/index.js'
+import router from './router.js'
+import axios from 'axios'
+import qs from 'qs'
+
+//设置跨域请求
+axios.defaults.baseURL = "/api"
 
 Vue.config.productionTip = false
+Vue.prototype.$axios = axios //全局导入
+//参数转换,让服务器可以接收到post请求参数
+Vue.prototype.$qs = qs //全局导入
 
 new Vue({
   vuetify,
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,19 @@
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+  devServer: {
+      proxy: {
+  	//在当前的项目中如果输入了/api这个路径，那么就相当于请求了target的目录路径
+        '/api': {
+  		  //被代理的目标服务器
+          target: 'http://localhost:8080', 
+          changeOrigin: true,  //运行跨域
+  		//路径重写
+  		pathRewrite: {
+  			'^/api': '/'
+  		}
+        }
+      }
+    }
 }
